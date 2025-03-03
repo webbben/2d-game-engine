e964a800ef6751b37ddf3b40d59b9f51ab4725f5
Ben Webb <ben.webb340@gmail.com>
2025-03-03 20:58:56 +0900

    trim image dimensions down to exact size of sprite

--- tileset/sprites.go
+++ tileset/sprites.go
@@ -22,6 +22,9 @@ type SpriteComponentPaths struct {
 	Shadow SpriteComponent
 }
 
+// BuildSpriteFrameImage builds a single frame image, given the component images.
+//
+// Note: This function is a bit oddly made as of now. It probably could be simplified, but for now I'll leave it as is since it works.
 func BuildSpriteFrameImage(components SpriteComponentPaths) (*ebiten.Image, error) {
 	// load component images
 	skinImage, _, err := ebitenutil.NewImageFromFile(components.Skin.ImagePath)
@@ -59,55 +62,65 @@ func BuildSpriteFrameImage(components SpriteComponentPaths) (*ebiten.Image, erro
 	if legsImage.Bounds().Dx() > maxWidth {
 		maxWidth = legsImage.Bounds().Dx()
 	}
-
-	baseWidth := skinImage.Bounds().Dx() * 2
 	baseHeight := skinImage.Bounds().Dy() * 2
 
-	// combine all components into result image
-	result := ebiten.NewImage(baseWidth, baseHeight+2) // +2: adds a little extra space at the bottom for the shadow
-	result.Fill(color.RGBA{0, 255, 0, 255})            // Green background
-
-	baseX := float64(baseWidth / 4)
-
 	// the center x position that all component centers must match to
-	centerX := baseX + (float64(maxWidth) / 2)
+	centerX := float64(maxWidth) / 2
 
 	// Skin
 	y := float64(baseHeight-skinImage.Bounds().Dy()) + float64(components.Skin.Dy)
-	dx := centerX - (float64(skinImage.Bounds().Dx()/2) + baseX) // difference in center of component vs global center
-	x := baseX + dx + float64(components.Skin.Dx)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(x, y)
-	result.DrawImage(skinImage, op)
+	dx := centerX - float64(skinImage.Bounds().Dx()/2) // difference in center of component vs global center
+	x := dx + float64(components.Skin.Dx) - 1
+	skinX, skinY := x, y
 
 	// Legs
 	y = float64(baseHeight-legsImage.Bounds().Dy()) + float64(components.Legs.Dy)
-	dx = centerX - (float64(legsImage.Bounds().Dx()/2) + baseX)
-	x = baseX + dx + float64(components.Legs.Dx)
-	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(x, y)
-	result.DrawImage(legsImage, op)
+	dx = centerX - float64(legsImage.Bounds().Dx()/2)
+	x = dx + float64(components.Legs.Dx) - 1
+	legsX, legsY := x, y
 
 	// Body
 	y -= float64(bodyImage.Bounds().Dy()) + float64(components.Body.Dy)
-	dx = centerX - (float64(bodyImage.Bounds().Dx()/2) + baseX)
-	x = baseX + dx + float64(components.Body.Dx)
-	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(x, y)
-	result.DrawImage(bodyImage, op)
+	dx = centerX - float64(bodyImage.Bounds().Dx()/2)
+	x = dx + float64(components.Body.Dx) - 1
+	bodyX, bodyY := x, y
 
+	// Head
 	y -= float64(headImage.Bounds().Dy()) + float64(components.Head.Dy)
-	dx = centerX - (float64(headImage.Bounds().Dx()/2) + baseX)
-	x = baseX + dx + float64(components.Head.Dx)
-	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(x, y)
-	result.DrawImage(headImage, op)
+	dx = centerX - float64(headImage.Bounds().Dx()/2)
+	x = dx + float64(components.Head.Dx) - 1
+	headX, headY := x, y
 
 	// put shadow on last, starting from the bottom again
 	// putting on last since shadow may cover other things, like clothes too
 	y = float64(baseHeight-shadowImage.Bounds().Dy()) + 2 // +2: shadow goes a little below the rest of the sprite
+	dx = centerX - float64(shadowImage.Bounds().Dx()/2)
+	x = dx + float64(components.Shadow.Dx) - 1
+	shadowX, shadowY := x, y
+
+	// We've calculated all the dimensions, now actually put it all together
+	totalHeight := float64(baseHeight+2) - headY
+	result := ebiten.NewImage(maxWidth, int(totalHeight)) // +2: adds a little extra space at the bottom for the shadow
+	result.Fill(color.RGBA{0, 255, 0, 255})               // Green background
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(skinX, skinY-headY)
+	result.DrawImage(skinImage, op)
+
 	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(x, y)
+	op.GeoM.Translate(legsX, legsY-headY)
+	result.DrawImage(legsImage, op)
+
+	op = &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(bodyX, bodyY-headY)
+	result.DrawImage(bodyImage, op)
+
+	op = &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(headX, headY-headY)
+	result.DrawImage(headImage, op)
+
+	op = &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(shadowX, shadowY-headY)
 	result.DrawImage(shadowImage, op)
 
 	return result, nil
