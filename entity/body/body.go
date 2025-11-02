91271a05b522ae715c260ffab517a0e2c8f832a2
Ben Webb <ben.webb340@gmail.com>
2025-11-02 22:25:09 +0900

    add JSON export feature to body

--- entity/body/body.go
+++ entity/body/body.go
@@ -1,7 +1,11 @@
 package body
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/webbben/2d-game-engine/internal/config"
@@ -24,43 +28,74 @@ type EntityBodySet struct {
 
 	// actual body definition - not including equiped items
 
-	bodyHSV HSV
-	bodySet BodyPartSet
-	eyesHSV HSV
-	eyesSet BodyPartSet
-	hairHSV HSV
-	hairSet BodyPartSet
-	armsSet BodyPartSet
+	BodyHSV HSV
+	BodySet BodyPartSet
+	EyesHSV HSV
+	EyesSet BodyPartSet
+	HairHSV HSV
+	HairSet BodyPartSet
+	ArmsSet BodyPartSet
 
 	// currently equiped items
 
-	equipBodySet BodyPartSet
-	equipHeadSet BodyPartSet
-	weaponSet    BodyPartSet
-	weaponFxSet  BodyPartSet
+	EquipBodySet BodyPartSet `json:"-"`
+	EquipHeadSet BodyPartSet `json:"-"`
+	WeaponSet    BodyPartSet `json:"-"`
+	WeaponFxSet  BodyPartSet `json:"-"`
 
 	stretchX, stretchY int     `json:"-"` // amount to stretch (non-body) parts by, when body is larger or smaller
 	globalOffsetY      float64 `json:"-"` // amount to offset placement of (non-body) parts by, when body is taller or shorter
 	nonBodyYOffset     int     `json:"-"`
 }
 
+func ReadJSON(jsonFilePath string) (EntityBodySet, error) {
+	if !config.FileExists(jsonFilePath) {
+		return EntityBodySet{}, errors.New("no file found at path: " + jsonFilePath)
+	}
+
+	data, err := os.ReadFile(jsonFilePath)
+	if err != nil {
+		return EntityBodySet{}, fmt.Errorf("failed to read file data: %w", err)
+	}
+
+	var eb EntityBodySet
+	err = json.Unmarshal(data, &eb)
+	if err != nil {
+		return EntityBodySet{}, fmt.Errorf("failed to unmarshal data: %w", err)
+	}
+	return eb, nil
+}
+
+func (eb EntityBodySet) WriteToJSON(outputFilePath string) error {
+	if !filepath.IsAbs(outputFilePath) {
+		return fmt.Errorf("given path is not abs (%s); please pass an absolute path", outputFilePath)
+	}
+
+	data, err := json.MarshalIndent(eb, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+
+	return os.WriteFile(outputFilePath, data, 0644)
+}
+
 func (eb *EntityBodySet) SetBodyHSV(h, s, v float64) {
-	eb.bodyHSV = HSV{h, s, v}
+	eb.BodyHSV = HSV{h, s, v}
 }
 func (eb EntityBodySet) GetBodyHSV() (h, s, v float64) {
-	return eb.bodyHSV.H, eb.bodyHSV.S, eb.bodyHSV.V
+	return eb.BodyHSV.H, eb.BodyHSV.S, eb.BodyHSV.V
 }
 func (eb *EntityBodySet) SetEyesHSV(h, s, v float64) {
-	eb.eyesHSV = HSV{h, s, v}
+	eb.EyesHSV = HSV{h, s, v}
 }
 func (eb EntityBodySet) GetEyesHSV() (h, s, v float64) {
-	return eb.eyesHSV.H, eb.eyesHSV.S, eb.eyesHSV.V
+	return eb.EyesHSV.H, eb.EyesHSV.S, eb.EyesHSV.V
 }
 func (eb *EntityBodySet) SetHairHSV(h, s, v float64) {
-	eb.hairHSV = HSV{h, s, v}
+	eb.HairHSV = HSV{h, s, v}
 }
 func (eb EntityBodySet) GetHairHSV() (h, s, v float64) {
-	return eb.hairHSV.H, eb.hairHSV.S, eb.hairHSV.V
+	return eb.HairHSV.H, eb.HairHSV.S, eb.HairHSV.V
 }
 
 // creates a base body set, without anything equiped
@@ -88,37 +123,24 @@ func NewEntityBodySet(bodySet, armsSet, hairSet, eyesSet, equipHeadSet, equipBod
 		animation:          "",
 		animationTickCount: 15,
 		currentDirection:   'D',
-		bodySet:            bodySet,
-		bodyHSV:            *bodyHSV,
-		armsSet:            armsSet,
-		hairSet:            hairSet,
-		hairHSV:            *hairHSV,
-		eyesSet:            eyesSet,
-		eyesHSV:            *eyesHSV,
-		equipBodySet:       equipBodySet,
-		equipHeadSet:       equipHeadSet,
-		weaponSet:          weaponSet,
-		weaponFxSet:        weaponFxSet,
+		BodySet:            bodySet,
+		BodyHSV:            *bodyHSV,
+		ArmsSet:            armsSet,
+		HairSet:            hairSet,
+		HairHSV:            *hairHSV,
+		EyesSet:            eyesSet,
+		EyesHSV:            *eyesHSV,
+		EquipBodySet:       equipBodySet,
+		EquipHeadSet:       equipHeadSet,
+		WeaponSet:          weaponSet,
+		WeaponFxSet:        weaponFxSet,
 	}
 
 	return eb
 }
 
-func (eb EntityBodySet) GetCodeDefinition() string {
-	eb.bodySet.unsetAllImages()
-	eb.hairSet.unsetAllImages()
-	eb.eyesSet.unsetAllImages()
-	eb.armsSet.unsetAllImages()
-	eb.equipBodySet.unsetAllImages()
-	eb.equipHeadSet.unsetAllImages()
-	eb.weaponSet.unsetAllImages()
-	eb.weaponFxSet.unsetAllImages()
-
-	return fmt.Sprintf("%#v", eb)
-}
-
 func (eb *EntityBodySet) Dimensions() (dx, dy int) {
-	bounds := eb.bodySet.img.Bounds()
+	bounds := eb.BodySet.img.Bounds()
 	return bounds.Dx(), bounds.Dy()
 }
 
@@ -130,40 +152,40 @@ func (eb *EntityBodySet) SetBody(bodyDef, armDef SelectedPartDef) {
 		panic("arms must be defined")
 	}
 
-	eb.bodySet.setImageSource(bodyDef, 0, 0)
+	eb.BodySet.setImageSource(bodyDef, 0, 0)
 
 	// arms are directly set with body
-	eb.armsSet.setImageSource(armDef, 0, 0)
+	eb.ArmsSet.setImageSource(armDef, 0, 0)
 
 	eb.stretchX = bodyDef.StretchX
 	eb.stretchY = bodyDef.StretchY
 	eb.globalOffsetY = float64(bodyDef.OffsetY)
 
 	// reload any body parts that are influenced by stretch properties
-	eb.hairSet.load(eb.stretchX, 0)
-	eb.equipHeadSet.load(eb.stretchX, 0)
-	eb.equipBodySet.load(eb.stretchX, eb.stretchY)
+	eb.HairSet.load(eb.stretchX, 0)
+	eb.EquipHeadSet.load(eb.stretchX, 0)
+	eb.EquipBodySet.load(eb.stretchX, eb.stretchY)
 }
 
 func (eb *EntityBodySet) SetEyes(def SelectedPartDef) {
 	if def.None {
 		panic("eyes must be defined")
 	}
-	eb.eyesSet.setImageSource(def, 0, 0)
+	eb.EyesSet.setImageSource(def, 0, 0)
 }
 
 func (eb *EntityBodySet) SetHair(def SelectedPartDef) {
-	eb.hairSet.setImageSource(def, eb.stretchX, 0)
+	eb.HairSet.setImageSource(def, eb.stretchX, 0)
 	if eb.cropHairToHead {
 		eb.cropHair()
 	}
 }
 
 func (eb *EntityBodySet) SetEquipHead(def SelectedPartDef) {
-	eb.equipHeadSet.setImageSource(def, eb.stretchX, 0)
+	eb.EquipHeadSet.setImageSource(def, eb.stretchX, 0)
 
 	// since some head equipment may cause hair to be cropped, always reload hair when head is equiped
-	eb.hairSet.load(eb.stretchX, 0)
+	eb.HairSet.load(eb.stretchX, 0)
 
 	if def.CropHairToHead {
 		eb.cropHair()
@@ -171,18 +193,18 @@ func (eb *EntityBodySet) SetEquipHead(def SelectedPartDef) {
 }
 
 func (eb *EntityBodySet) SetEquipBody(def SelectedPartDef) {
-	eb.equipBodySet.setImageSource(def, eb.stretchX, eb.stretchY)
+	eb.EquipBodySet.setImageSource(def, eb.stretchX, eb.stretchY)
 }
 
 func (eb *EntityBodySet) SetWeapon(weaponDef, weaponFxDef SelectedPartDef) {
-	if eb.weaponSet.None {
+	if eb.WeaponSet.None {
 		panic("no weapon set!")
 	}
-	if eb.weaponFxSet.None {
+	if eb.WeaponFxSet.None {
 		panic("no weaponFx set!")
 	}
-	eb.weaponSet.setImageSource(weaponDef, 0, 0)
-	eb.weaponFxSet.setImageSource(weaponFxDef, 0, 0)
+	eb.WeaponSet.setImageSource(weaponDef, 0, 0)
+	eb.WeaponFxSet.setImageSource(weaponFxDef, 0, 0)
 }
 
 func (eb EntityBodySet) GetCurrentAnimation() string {
@@ -376,13 +398,13 @@ func stretchImage(img *ebiten.Image, stretchX, stretchY int) *ebiten.Image {
 
 func (eb *EntityBodySet) cropHair() {
 	leftHead := ebiten.NewImage(config.TileSize, config.TileSize)
-	leftHead.DrawImage(eb.bodySet.WalkAnimation.L[0], nil)
+	leftHead.DrawImage(eb.BodySet.WalkAnimation.L[0], nil)
 	rightHead := ebiten.NewImage(config.TileSize, config.TileSize)
-	rightHead.DrawImage(eb.bodySet.WalkAnimation.R[0], nil)
+	rightHead.DrawImage(eb.BodySet.WalkAnimation.R[0], nil)
 	upHead := ebiten.NewImage(config.TileSize, config.TileSize)
-	upHead.DrawImage(eb.bodySet.WalkAnimation.U[0], nil)
+	upHead.DrawImage(eb.BodySet.WalkAnimation.U[0], nil)
 	downHead := ebiten.NewImage(config.TileSize, config.TileSize)
-	downHead.DrawImage(eb.bodySet.WalkAnimation.D[0], nil)
+	downHead.DrawImage(eb.BodySet.WalkAnimation.D[0], nil)
 
 	cropper := func(a *Animation) {
 		for i, img := range a.L {
@@ -399,9 +421,9 @@ func (eb *EntityBodySet) cropHair() {
 		}
 	}
 
-	cropper(&eb.hairSet.WalkAnimation)
-	cropper(&eb.hairSet.RunAnimation)
-	cropper(&eb.hairSet.SlashAnimation)
+	cropper(&eb.HairSet.WalkAnimation)
+	cropper(&eb.HairSet.RunAnimation)
+	cropper(&eb.HairSet.SlashAnimation)
 }
 
 func (set *BodyPartSet) setCurrentFrame(dir byte, animationName string) {
@@ -473,44 +495,44 @@ func (eb *EntityBodySet) Draw(screen *ebiten.Image, x, y, characterScale float64
 	yOff := eb.globalOffsetY * characterScale
 	characterTileSize := config.TileSize * characterScale
 	// Body
-	rendering.DrawHSVImage(screen, eb.bodySet.img, eb.bodyHSV.H, eb.bodyHSV.S, eb.bodyHSV.V, bodyX, bodyY, characterScale)
+	rendering.DrawHSVImage(screen, eb.BodySet.img, eb.BodyHSV.H, eb.BodyHSV.S, eb.BodyHSV.V, bodyX, bodyY, characterScale)
 	// Arms
-	rendering.DrawHSVImage(screen, eb.armsSet.img, eb.bodyHSV.H, eb.bodyHSV.S, eb.bodyHSV.V, bodyX, bodyY, characterScale)
+	rendering.DrawHSVImage(screen, eb.ArmsSet.img, eb.BodyHSV.H, eb.BodyHSV.S, eb.BodyHSV.V, bodyX, bodyY, characterScale)
 	// Equip Body
 	equipBodyYOffset := 0.0
 	if eb.stretchY%2 != 0 {
 		// if stretchY is an odd number, offset equip body by -1
 		equipBodyYOffset = -characterScale
 	}
-	rendering.DrawImage(screen, eb.equipBodySet.img, bodyX, bodyY+yOff+equipBodyYOffset, characterScale)
+	rendering.DrawImage(screen, eb.EquipBodySet.img, bodyX, bodyY+yOff+equipBodyYOffset, characterScale)
 
 	// Eyes
 	eyesX := bodyX
 	eyesY := bodyY + (float64(eb.nonBodyYOffset) * characterScale) + yOff
-	if eb.eyesSet.img != nil {
-		rendering.DrawHSVImage(screen, eb.eyesSet.img, eb.eyesHSV.H, eb.eyesHSV.S, eb.eyesHSV.V, eyesX, eyesY, characterScale)
+	if eb.EyesSet.img != nil {
+		rendering.DrawHSVImage(screen, eb.EyesSet.img, eb.EyesHSV.H, eb.EyesHSV.S, eb.EyesHSV.V, eyesX, eyesY, characterScale)
 	}
 	// Hair
 	hairY := bodyY + (float64(eb.nonBodyYOffset) * characterScale) + yOff
-	if eb.hairSet.img == nil {
+	if eb.HairSet.img == nil {
 		panic("hair img is nil")
 	}
-	rendering.DrawHSVImage(screen, eb.hairSet.img, eb.hairHSV.H, eb.hairHSV.S, eb.hairHSV.V, bodyX, hairY, characterScale)
+	rendering.DrawHSVImage(screen, eb.HairSet.img, eb.HairHSV.H, eb.HairHSV.S, eb.HairHSV.V, bodyX, hairY, characterScale)
 
 	// Equip Head
-	if eb.equipHeadSet.img != nil {
-		rendering.DrawImage(screen, eb.equipHeadSet.img, bodyX, hairY, characterScale)
+	if eb.EquipHeadSet.img != nil {
+		rendering.DrawImage(screen, eb.EquipHeadSet.img, bodyX, hairY, characterScale)
 	}
 
 	// Equip Weapon
-	if eb.weaponSet.img != nil {
+	if eb.WeaponSet.img != nil {
 		// weapons are in 80x80 (5 tiles width & height) tiles
 		// this is to accomodate for the extra space they need for their swings and stuff
 		weaponY := bodyY - (characterTileSize) + yOff
 		weaponX := bodyX - (characterTileSize * 2)
-		rendering.DrawImage(screen, eb.weaponSet.img, weaponX, weaponY, characterScale)
-		if eb.weaponFxSet.img != nil {
-			rendering.DrawImage(screen, eb.weaponFxSet.img, weaponX, weaponY, characterScale)
+		rendering.DrawImage(screen, eb.WeaponSet.img, weaponX, weaponY, characterScale)
+		if eb.WeaponFxSet.img != nil {
+			rendering.DrawImage(screen, eb.WeaponFxSet.img, weaponX, weaponY, characterScale)
 		}
 	}
 }
@@ -521,41 +543,41 @@ func (eb *EntityBodySet) Update() {
 		if eb.ticks > eb.animationTickCount {
 			// SETS: next frame
 			eb.ticks = 0
-			eb.bodySet.nextFrame(eb.animation)
-			eb.armsSet.nextFrame(eb.animation)
-			eb.equipBodySet.nextFrame(eb.animation)
-			eb.equipHeadSet.nextFrame(eb.animation)
-			eb.weaponSet.nextFrame(eb.animation)
-			eb.weaponFxSet.nextFrame(eb.animation)
+			eb.BodySet.nextFrame(eb.animation)
+			eb.ArmsSet.nextFrame(eb.animation)
+			eb.EquipBodySet.nextFrame(eb.animation)
+			eb.EquipHeadSet.nextFrame(eb.animation)
+			eb.WeaponSet.nextFrame(eb.animation)
+			eb.WeaponFxSet.nextFrame(eb.animation)
 		}
 	}
 
 	// SETS: get current frame
-	eb.bodySet.setCurrentFrame(eb.currentDirection, eb.animation)
-	eb.eyesSet.setCurrentFrame(eb.currentDirection, eb.animation)
-	eb.hairSet.setCurrentFrame(eb.currentDirection, eb.animation)
-	eb.armsSet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.BodySet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.EyesSet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.HairSet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.ArmsSet.setCurrentFrame(eb.currentDirection, eb.animation)
 
-	eb.equipBodySet.setCurrentFrame(eb.currentDirection, eb.animation)
-	eb.equipHeadSet.setCurrentFrame(eb.currentDirection, eb.animation)
-	eb.weaponSet.setCurrentFrame(eb.currentDirection, eb.animation)
-	eb.weaponFxSet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.EquipBodySet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.EquipHeadSet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.WeaponSet.setCurrentFrame(eb.currentDirection, eb.animation)
+	eb.WeaponFxSet.setCurrentFrame(eb.currentDirection, eb.animation)
 
-	eb.nonBodyYOffset = eb.bodySet.getCurrentYOffset(eb.animation)
+	eb.nonBodyYOffset = eb.BodySet.getCurrentYOffset(eb.animation)
 }
 
 func (eb *EntityBodySet) SetAnimation(animation string) {
 	eb.animation = animation
 
 	// SETS: reset animation index
-	eb.bodySet.animIndex = 0
-	eb.eyesSet.animIndex = 0
-	eb.hairSet.animIndex = 0
-	eb.armsSet.animIndex = 0
-	eb.equipBodySet.animIndex = 0
-	eb.equipHeadSet.animIndex = 0
-	eb.weaponSet.animIndex = 0
-	eb.weaponFxSet.animIndex = 0
+	eb.BodySet.animIndex = 0
+	eb.EyesSet.animIndex = 0
+	eb.HairSet.animIndex = 0
+	eb.ArmsSet.animIndex = 0
+	eb.EquipBodySet.animIndex = 0
+	eb.EquipHeadSet.animIndex = 0
+	eb.WeaponSet.animIndex = 0
+	eb.WeaponFxSet.animIndex = 0
 }
 
 func (eb *EntityBodySet) RotateLeft() {
@@ -586,25 +608,25 @@ func (eb *EntityBodySet) RotateRight() {
 
 func (eb *EntityBodySet) SetDirection(dir byte) {
 	// SETS: reset animation index
-	eb.bodySet.animIndex = 0
-	eb.eyesSet.animIndex = 0
-	eb.hairSet.animIndex = 0
-	eb.armsSet.animIndex = 0
+	eb.BodySet.animIndex = 0
+	eb.EyesSet.animIndex = 0
+	eb.HairSet.animIndex = 0
+	eb.ArmsSet.animIndex = 0
 
-	eb.equipBodySet.animIndex = 0
-	eb.equipHeadSet.animIndex = 0
-	eb.weaponSet.animIndex = 0
-	eb.weaponFxSet.animIndex = 0
+	eb.EquipBodySet.animIndex = 0
+	eb.EquipHeadSet.animIndex = 0
+	eb.WeaponSet.animIndex = 0
+	eb.WeaponFxSet.animIndex = 0
 
 	eb.currentDirection = dir
 
 	// SETS: set to first frame of walking animation
-	eb.bodySet.setCurrentFrame(dir, ANIM_WALK)
-	eb.eyesSet.setCurrentFrame(dir, ANIM_WALK)
-	eb.hairSet.setCurrentFrame(dir, ANIM_WALK)
-	eb.armsSet.setCurrentFrame(dir, ANIM_WALK)
-	eb.equipBodySet.setCurrentFrame(dir, ANIM_WALK)
-	eb.equipHeadSet.setCurrentFrame(dir, ANIM_WALK)
-	eb.weaponSet.setCurrentFrame(dir, ANIM_WALK)
-	eb.weaponFxSet.setCurrentFrame(dir, ANIM_WALK)
+	eb.BodySet.setCurrentFrame(dir, ANIM_WALK)
+	eb.EyesSet.setCurrentFrame(dir, ANIM_WALK)
+	eb.HairSet.setCurrentFrame(dir, ANIM_WALK)
+	eb.ArmsSet.setCurrentFrame(dir, ANIM_WALK)
+	eb.EquipBodySet.setCurrentFrame(dir, ANIM_WALK)
+	eb.EquipHeadSet.setCurrentFrame(dir, ANIM_WALK)
+	eb.WeaponSet.setCurrentFrame(dir, ANIM_WALK)
+	eb.WeaponFxSet.setCurrentFrame(dir, ANIM_WALK)
 }
